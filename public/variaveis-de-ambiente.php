50c2bdfbbd2b25780bdf416679e8524026d8d863
Lucas-TA <dev.ascencao@gmail.com>
2024-08-05 16:08:49 -0300

    modificando as variaveis de ambiente

--- public/variaveis-de-ambiente.php
+++ public/variaveis-de-ambiente.php
@@ -2,9 +2,9 @@
 //Instalar o seguinte repositório para configurar as variáveis de ambiente
 //https://github.com/vlucas/phpdotenv
 //Para utilizar as variáveis de ambiente, acrescente o código a seguir:
-    require '../vendor/autoload.php';
-
-    $dotenv = Dotenv\Dotenv::createImmutable('../');
-    $dotenv->load();
-
-    var_dump($_ENV['DATABASE']);
+//    require '../vendor/autoload.php';
+//
+//    $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+//    $dotenv->load();
+//
+//    var_dump($_ENV['KEY']);
